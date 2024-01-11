9235928d1e89af0e8bd21e921b9402e496a8ba41
surapong.n <surapong.n@nida.ac.th>
2024-01-11 12:49:32 +0700

    update config indent

--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -1,29 +1,32 @@
 local options = {
-  ensure_installed = {
-    "vim",
-    "lua",
-    "html",
-    "htmldjango",
-    "css",
-    "javascript",
-    "typescript",
-    "tsx",
-    "c",
-    "markdown",
-    "markdown_inline",
-    "python",
-    "go",
-    "bash",
-    "dockerfile",
-  },
-  auto_install = true,
-  highlight = {
-    enable = true,
-    use_languagetree = true,
-  },
-  indent = {
-    enable = false,
-  },
+	ensure_installed = {
+		"vim",
+		"lua",
+		"html",
+		"htmldjango",
+		"css",
+		"javascript",
+		"typescript",
+		"tsx",
+		"c",
+		"markdown",
+		"markdown_inline",
+		"python",
+		"go",
+		"bash",
+		"dockerfile",
+	},
+	auto_install = true,
+	highlight = {
+		enable = true,
+		use_languagetree = true,
+	},
+	indent = {
+		enable = true,
+		-- disable = {
+		-- 	"python",
+		-- },
+	},
 }
 
 return options
