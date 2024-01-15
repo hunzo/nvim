cfc7642427831968addcb90bc45226d4c46a76f6
surapong.n <surapong.n@nida.ac.th>
2024-01-15 09:42:56 +0700

    update

--- install-software.sh
+++ install-software.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 # install list
-# - golang
-# - python
-# - nodejs
+# - Golang
+# - Python
+# - Python-venv
+# - Node Version Manager (nvm)
+
+GO_VERSION=go1.21.5
+NVIM_VERSION=v0.9.4
+NVM_VERSION=v0.39.7
 
 # -- install golang
-GO_VERSION=go1.21.5
 curl -OL https://golang.org/dl/$GO_VERSION.linux-amd64.tar.gz
 
 mkdir ./.sdk
 mkdir ./.go
 
 tar xzvf $GO_VERSION.linux-amd64.tar.gz -C ./.sdk
-test 
+
 sh -c "cat >> ~/.bashrc << 'EOF'
 # golang setting
 export GOPATH=\$HOME/.go
@@ -23,7 +27,7 @@ EOF"
 
 rm $GO_VERSION.linux-amd64.tar.gz
 
-# set shell
+# -- set shell .bashrc
 sh -c "cat >> ~/.bashrc << 'EOF'
 show_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
@@ -31,8 +35,7 @@ show_git_branch() {
 export PS1='\u@\h \[\e[32m\]\w \[\e[91m\]\$(show_git_branch)\[\e[00m\]$ '
 EOF"
 
-# clear nvim
-#
+# -- clear old neovim
 rm -rf ~/.config/nvim
 
 # optional but recommended
@@ -48,24 +51,25 @@ alias vim=nvim
 set -o vi
 EOF"
 
-curl -OL https://github.com/neovim/neovim/releases/download/v0.9.4/nvim-linux64.tar.gz
+
+# -- install neovim
+curl -OL https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/nvim-linux64.tar.gz
 
 sudo tar xzvf nvim-linux64.tar.gz -C /usr/bin/
 sudo ln -s /usr/bin/nvim-linux64/bin/nvim /usr/bin/nvim
 
 rm nvim-linux64.tar.gz
-# -- python-venv
+
+
+# -- install python-venv
 sudo apt install python3-venv -y
 sudo apt install unzip -y
 sudo apt install build-essential -y
 
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+# -- install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VERSION/install.sh | bash
 source ~/.nvm/nvm.sh
 source ~/.bashrc
 
 # -- install nodejs
 nvm install --lts
-
-# -- config nvim 
-git clone https://github.com/hunzo/nvim ~/.config/nvim && nvim 
