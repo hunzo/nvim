56a265ec10f74287fe8f5682a6267be5bd87d2f5
surapong.n <surapong.n@nida.ac.th>
2024-01-16 15:22:04 +0700

    update yamlfmt

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,69 +1,70 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-    local mason_tool_installer = require("mason-tool-installer")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
+		mason_lspconfig.setup({
 
-      -- list of servers for mason to install
-      ensure_installed = {
-        "tailwindcss",
-        "tsserver",
-        "gopls",
-        "pyright",
-        "emmet_ls",
-        "html",
-        "lua_ls",
-        "yamlls",
-        "denols",
-        "dockerls",
-        "bashls",
-        "jsonls",
-        "cssls",
-      },
+			-- list of servers for mason to install
+			ensure_installed = {
+				"tailwindcss",
+				"tsserver",
+				"gopls",
+				"pyright",
+				"emmet_ls",
+				"html",
+				"lua_ls",
+				"yamlls",
+				-- "denols",
+				"dockerls",
+				"bashls",
+				"jsonls",
+				"cssls",
+			},
 
-      -- -- auto-install configured servers (with lspconfig)
-      automatic_installation = true, -- not the same as ensure_installed
-    })
+			-- -- auto-install configured servers (with lspconfig)
+			automatic_installation = true, -- not the same as ensure_installed
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "autopep8",
-        "djlint",
-        "pylint",
-        "eslint_d",
-        "prettier",
-        "stylua",
-        "gofumpt",
-        "golines",
-        "goimports-reviser",
-        "isort",
-        "ruff",
-        -- "golangcli_lint",
-        -- "mypy",
-        -- "black",
-      },
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"autopep8",
+				"djlint",
+				"pylint",
+				"eslint_d",
+				"prettier",
+				"stylua",
+				"gofumpt",
+				"golines",
+				"goimports-reviser",
+				"isort",
+				"ruff",
+				"yamlfmt",
+				-- "golangcli_lint",
+				-- "mypy",
+				-- "black",
+			},
+		})
+	end,
 }
