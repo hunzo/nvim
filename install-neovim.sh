b74dbb8c40cbf149da79415128946867da881d81
Ubuntu <idtadmin@nvim-config.dev.local>
2025-05-15 14:31:17 +0700

    update

--- install-neovim.sh
+++ install-neovim.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-NEOVIM_VERSION=v0.10.1
 
-# clear neovim
+NVIM_VERSION=v0.11.1
+NVIM_FILE_NAME=nvim-linux-x86_64
+
+# -- clear old neovim
+rm -rf ~/.config/nvim
+
+# optional but recommended
+rm -rf ~/.local/share/nvim
+rm -rf ~/.local/state/nvim
+rm -rf ~/.cache/nvim
 sudo rm -rf /usr/bin/nvim*
 
-curl -OL https://github.com/neovim/neovim/releases/download/$NEOVIM_VERSION/nvim-linux64.tar.gz
-
-sudo tar xzvf nvim-linux64.tar.gz -C /usr/bin/
-sudo ln -s /usr/bin/nvim-linux64/bin/nvim /usr/bin/nvim
-
-rm nvim-linux64.tar.gz
-
+# -- add .bashrc
 sh -c "cat >> ~/.bashrc << EOF
 export EDITOR=nvim
 alias vim=nvim
 set -o vi
 EOF"
+
+# -- install neovim
+curl -OL https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/$NVIM_FILE_NAME.tar.gz
+
+sudo rm /usr/bin/nvim
+sudo tar xzvf $NVIM_FILE_NAME.tar.gz -C /usr/bin/
+sudo ln -s /usr/bin/$NVIM_FILE_NAME/bin/nvim /usr/bin/nvim
+
+rm $NVIM_FILE_NAME.tar.gz
+
